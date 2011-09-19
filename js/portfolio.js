1beabcec28f6f84ea632b778de023e65ea8ea916
David Hazinski <dhazinski@gmail.com>
2011-09-19 02:30:00 -0400

    Making slideshow work.

--- js/portfolio.js
+++ js/portfolio.js
@@ -4,9 +4,12 @@ $(function(){
         var $thumbs = $('#thumbs');
 
         $thumbs.height($($thumbs.children().get(0)).height());
+
+        Galleria.loadTheme('js/galleria/themes/classic/galleria.classic.min.js');
     },
 
     bindEvents = function() {
+        var clicks = 0;
 
         $('#side-nav a').click(function(event){
             var $target = $(event.target.hash),
@@ -16,11 +19,43 @@ $(function(){
 
             $thumbs.height($target.height());
             $thumbs.scrollTop($target.position().top + $thumbs.scrollTop());
+        });
 
-            //$thumbs.animate({
-            //    height: $target.height(),
-            //    scrollTop: $target.position().top + $thumbs.scrollTop()
-            //}, 500);
+        $('#thumbs li a').click(function(event){
+            var url = event.target.parentNode.href;
+
+            event.preventDefault();
+
+            $.get(url, function(data) {
+                var $images = $(data).find('#project img'),
+                    $exit = $('.exit-button'),
+                    prefix = /.+\//.exec(url)[0],
+                    data = [];
+
+                $images.each(function(){
+                    data.push({
+                        image: prefix + $(this).attr('src')
+                    });
+                });
+
+                $('#galleria').galleria({
+                    dataSource: data
+                });
+
+                gal = Galleria.get(clicks++)
+                    .bind(Galleria.FULLSCREEN_ENTER, function(e){
+                        $exit.show();
+                    })
+                    .bind(Galleria.FULLSCREEN_EXIT, function(e){
+                        $exit.hide();
+                    })
+                    .enterFullscreen();
+
+                $exit.click(function(){
+                    gal.exitFullscreen();
+                });
+
+            });
         });
 
     };
